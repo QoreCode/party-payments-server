8b523ecbddb6a203cdd8283401035d1d9397b480
v.okrushko <v.okrushko@astoundcommerce.com>
2023-08-04 14:06:23 +0300

    feat: GET Events by User UID: PP-36

--- src/Entity/PartyEvent.php
+++ src/Entity/PartyEvent.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
@@ -14,8 +13,11 @@ use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Delete;
 use App\Repository\PartyEventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
@@ -27,9 +29,16 @@ use Symfony\Component\Validator\Constraints as Assert;
         new Delete(),
     ],
     formats: ['json'],
+    normalizationContext: ['groups' => 'event:read'],
+    denormalizationContext: [
+        'groups' => [
+            'event:create',
+            'event:update'
+        ]
+    ],
     collectDenormalizationErrors: true
 )]
-#[ApiFilter(filterClass: SearchFilter::class, properties: ['name' => 'partial'])]
+#[ApiFilter(filterClass: SearchFilter::class, properties: ['name' => 'partial', 'members.user'])]
 #[ApiFilter(RangeFilter::class, properties: ['date'])]
 #[ApiFilter(OrderFilter::class, properties: ['date' => 'ASC', 'name' => 'ASC'])]
 #[ORM\Entity(repositoryClass: PartyEventRepository::class)]
@@ -39,20 +48,28 @@ class PartyEvent
     #[ORM\Id]
     #[ORM\Column(length: 255, unique: true)]
     #[Assert\NotBlank]
+    #[Groups(['event:read', 'event:create'])]
     private string $uid;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
+    #[Groups(['event:read', 'event:create', 'event:update'])]
     private string $name;
 
     #[ORM\Column(type: Types::INTEGER)]
     #[Assert\NotBlank]
+    #[Groups(['event:read', 'event:create'])]
     private int $date;
+
+    #[ORM\OneToMany(mappedBy: 'event', targetEntity: Member::class)]
+    private Collection $members;
+
 
     public function __construct($uid, $date = null)
     {
         $this->uid = $uid;
         $this->date = $date ?? time();
+        $this->members = new ArrayCollection();
     }
 
     public function getUid(): string
@@ -82,5 +99,10 @@ class PartyEvent
         $this->date = $date;
 
         return $this;
+    }
+
+    public function getMembers(): Collection
+    {
+        return $this->members;
     }
 }
